24e98db557d44cc51521edad6108c30a677431ff
ostrichick <gip4kun@gmail.com>
2022-07-03 18:33:03 +0900

    pagination

--- Selfstudy/nomad_python/main.py
+++ Selfstudy/nomad_python/main.py
@@ -1,21 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 
-indeed_result = requests.get("https://www.indeed.com/jobs?q=python&limit=50")
+# indeed_result = requests.get("https://www.indeed.com/jobs?q=python&limit=50")
+indeed_result = requests.get("https://job.alio.go.kr/recruit.do?pageNo=1&idx=&recruitYear=&recruitMonth=&location=R3009&location=R3024&career=R2010&career=R2030&education=R7010&eduType=multi&s_date=2022.05.03&e_date=2022.07.03&org_type=&org_name=&ing=2&title=&order=REG_DATE")
 
 # print(indeed_result.text)
 
 indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
 # print(indeed_soup)
 
-pagination = indeed_soup.find("div", {"class":"pagination"})
+pagination = indeed_soup.find("div", {"class":"page"})
 
 # print(pagination)
-pages = pagination.find_all('a')
-spans = []
-# print(pages)
+links = pagination.find_all('a')
+pages = []
+# print(links)
 
-for page in pages:
-    # print(page.find("span"))
-    spans.append(page.find("span"))
-spans = spans[0:-1]
+for link in links[2:-2]:
+    # print(link.find("span"))
+    # pages.append(link.find("span").string)
+    pages.append(int(link.string))
+# pages = pages[0:-1]
+max_page = print(pages[-1])
+
+
